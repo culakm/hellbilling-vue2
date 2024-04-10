dc3719dabad187d72214d33c168639d89d3f2d96
Martin Culak <culak.martin@gmail.com>
2024-04-10 11:33:22 +0200

    nahodeny hellbilling

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import './assets/main.css'
+import { createApp, defineAsyncComponent } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index.js';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+const BaseDialog = defineAsyncComponent(() => import('./components/ui/BaseDialog.vue'));
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(router);
+app.use(store);
 
-app.use(router)
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
 
-app.mount('#app')
+app.mount('#app');
